ea402613a0ac43a7b4b06e39ba55c4d478721e2c
Am-aadvik <121937109+Am-aadvik@users.noreply.github.com>
2023-01-05 07:33:21 +0530

    Add files via upload

---
+++ CC_NTRIPLETS_01.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <math.h>
+#include <vector>
+using namespace std;
+
+int gcd(int a, int b)
+{
+    return b == 0 ? a : gcd(b, a % b);   
+}
+int main()
+{
+    int t;
+    cin >> t;
+    while(t--)
+    {
+        vector<int> nums;
+        nums.push_back(1);
+        int n;
+        cin >> n;
+        if(n < 6)
+            cout << -1 << endl;
+        else
+        {
+        for(int i = 2; i <= sqrt(n);++i)
+        {
+            if(n % i == 0 && i != (n / i))
+            {
+                nums.push_back(i);
+                nums.push_back(n / i);
+                break;
+            }
+        }
+        
+        if(nums.size() == 3)
+            cout << "1 " << nums[1] << " " << nums[2] << endl;
+        else
+        cout << "-1" << endl;
+        }
+    }
+    return 0;
+}
